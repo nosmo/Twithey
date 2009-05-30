798817c8b05c3f17a1500635e475941c7fe90a7c
Hugh Nowlan <nosmo@netsoc.tcd.ie>
2009-05-30 20:29:59 +0100

    Initial commit - time to clean this up

---
+++ tweetlog.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import BeautifulSoup
+import twithey
+
+DEFAULT_OUTPUT_ENCODING = "utf-8"
+
+username = ""
+password = ""
+
+class TimeLine:
+
+    tweets = []
+
+    def insertTweet(self, tweet):
+        self.tweets.append(tweet)
+
+    def __str__(self, encoding=DEFAULT_OUTPUT_ENCODING):
+        s = ""
+        for i in self.tweets:
+            s += str(i) + "\n\n"
+        return s
+
+class User:
+
+    userid=0
+    name=""
+    screen_name=""
+    location=""
+    description=""
+    url=""
+
+    def __init__(self, userid, aname, screen_name, location="", description="", url=""):
+        self.userid = userid
+        self.name = aname
+        self.screen_name = screen_name
+        self.location = location
+        self.description = description
+        self.url = url
+    
+class Tweet:
+
+    user = None
+    date = ""
+    tweetid = 0
+    message = ""
+
+    def __init__(self, user, date, tweetid, message):
+        self.user = user
+        self.date = date
+        self.tweetid = tweetid
+        self.message = message
+
+
+    def __str__(self, encoding=DEFAULT_OUTPUT_ENCODING):
+        """Make the Tweets actually look like hey messages.
+
+        I know it's stupid, you don't have to tell me
+        """
+
+        global username
+        
+        header = """Message from %s at %s
+%s sez:
+Tweet! %s
+
+""" % (self.user.screen_name, self.date,
+                        self.user.name, username)
+
+        footer = "\n\nEOF"
+
+        termwidth = 80
+        leftpadding = 8
+        messagepoint = 0
+        messagestr = " "*leftpadding
+        bits = self.message.split()
+        lines = []
+
+        for i in bits:
+            messagepoint += len(i)
+            if messagepoint > 80:
+                lines.append(messagestr)
+                messagestr = " "*leftpadding + i + " "
+                messagepoint = len(i)
+            else:
+                messagestr += i + " "
+        lines.append(messagestr)
+
+        return header + "\n".join(lines) + footer
+
+def parseTimeline():
+
+    timeline = TimeLine()
+
+    f = open("timeline.xml", "r")
+    data = f.read()
+    soup = BeautifulSoup.BeautifulStoneSoup(data)
+    statuses = soup.findAll("status")
+    for i in statuses:
+        date = i.created_at.string
+        msgid = i.id.string
+        message = i.text.string
+
+        user = i.find("user")
+        
+        thisuser = parseUser(user, i.user.find("name").string)
+        thistweet = Tweet(thisuser, date, msgid, message)
+
+        timeline.insertTweet(thistweet)
+
+    print timeline
+
+def parseUser(userdata, name):
+    
+    u = User(userdata.id.string,
+             name,
+             userdata.screen_name.string,
+             userdata.location.string,
+             userdata.description.string,
+             userdata.url.string)
+
+    return u
+
+def main():
+
+    global username
+    global password
+
+    (username, password, proto) = twithey.readConfig()
+
+    if not username or not password:
+        sys.stderr.write("Fill in the username and password variables you silly nonny\n")
+        sys.exit(1)
+
+    twithey.getTimeline(proto)
+    parseTimeline()
+
+
+if __name__ == "__main__":
+    main()
---
+++ twithey.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+
+import base64
+import ConfigParser
+import os
+import sys
+import urllib
+import urllib2
+
+username = ""
+password = ""
+proto = ""
+
+"""Twithey poster/getter.
+
+ These functions are used to post information to and get information
+ from Twitter. They're short and equal parts bitter and sweet.
+
+  getPage: Request a page using the user's details.
+  getTimeline(): Get the user's timeline
+  Post(): Post a public status message
+  directMessage(): Direct message a user
+"""
+
+__author__ = "nosmo@netsoc.tcd.ie"
+
+def readConfig(path=os.path.expanduser("~") + "/.twitheyrc"):
+    """Reads configuration information from the twitheyrc file.
+
+     path: an optional string pointing to the config file a user wishes to use
+    """
+
+    global username
+    global password
+    global proto
+
+    config = ConfigParser.ConfigParser()
+    config.read(os.path.expanduser("~") + "/.twitheyrc")
+
+    if not (config.has_option("account", "user") and config.has_option("account", "pass")):
+        sys.stderr.write("Config file has no username or password!")
+        sys.exit(4)
+
+    if config.get("connection", "https") == "yes":
+        proto = "https"
+    else:
+        proto = "http"
+
+    username = config.get("account", "user")
+    password = config.get("account", "pass")
+
+    return (username, password, proto)
+
+
+def getPage(url, data=None):
+    """Encode the user's login details and request a page.
+
+     url: a string containing the URL to be fetched
+     data: optional urlencode'd data dictionary
+    """
+    base64string = base64.encodestring('%s:%s' % (username, password))[:-1]
+    if data:
+        data = data + "&" + urllib.urlencode({"source": "twithey"})
+    else:
+        data =  urllib.urlencode({"source": "twithey"})
+    req = urllib2.Request(url, data)
+    req.add_header("Authorization", "Basic %s" % base64string)
+    try:
+        results = urllib2.build_opener().open(req).read()
+    except IOError, e:
+        print e.headers
+        sys.stderr.write("Uh oh, something screwed up when getting page!\n")
+        sys.exit(3)
+
+    return results
+
+def getTimeline(proto=None):
+    """Get the user's timeline."""
+    
+    theurl = '%s://twitter.com/statuses/friends_timeline.xml' % proto
+    results = getPage(theurl)
+    timeline = open("timeline.xml", "w")
+    timeline.write(results)
+    timeline.close()
+
+def chkLength(message):
+    length = 0
+    for i in message:
+        if i == "<" or i == ">":
+            length += 4
+        else:
+            length += 1
+    if length > 140:
+        return False
+    return True
+
+def Post():
+    """Make a public status update."""
+    
+    print "[ recipients : <Twitter> ]"
+
+    postdata = raw_input()
+
+    if not chkLength(postdata):
+        sys.stderr.write("Post too long!")
+        sys.exit(2)
+    
+    data = urllib.urlencode({'status' : postdata})
+    theurl = '%s://twitter.com/statuses/update.xml' % proto
+    if getPage(theurl, data):
+        print "[ tweet--okay. ]"
+    else:
+        print "Something went wrong along the way :("
+
+def directMessage(user):
+    """Direct message a user.
+
+     user: a username
+    """
+    
+    print "[ recipients : %s ]" % user
+    postdata = raw_input()
+
+    if not chkLength(postdata):
+        sys.stderr.write("Post too long!")
+        sys.exit(2)
+
+    data = urllib.urlencode({'user' : user, 'text' : postdata})
+    
+    theurl = '%s://twitter.com/direct_messages/new.xml' % proto
+    if getPage(theurl, data):
+        print "[ tweet %s--okay. ]" % user
+    else:
+        print "Something went wrong along the way :("
+
+if __name__ == "__main__":
+    """Yes, I know this is very basic."""
+
+    readConfig()
+
+    if not username or not password:
+        sys.stderr.write("Fill in the username and password variables you silly nonny\n")
+        sys.exit(1)
+        
+    if len(sys.argv) > 1:
+        directMessage(sys.argv[1])
+    else: 
+        Post()
